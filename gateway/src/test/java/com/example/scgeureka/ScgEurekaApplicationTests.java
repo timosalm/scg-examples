a9cea8ea363e1614198c015bf09442848ba6b233
Timo Salm <tsalm@pivotal.io>
2021-10-22 14:49:51 +0200

    Initial examples commit

---
+++ gateway/src/test/java/com/example/scgeureka/ScgEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scgeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScgEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
